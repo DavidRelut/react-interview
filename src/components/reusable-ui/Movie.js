52472f248e443fef89d5da7dfbf04d9d112bf40d
David R <codingaccelerator@gmail.com>
2022-10-27 13:40:11 +0200

    ✨💄 F03: MOVIES > Add logic & style for buttons toggle like/dislike + add data

--- src/components/reusable-ui/Movie.js
+++ src/components/reusable-ui/Movie.js
@@ -1,18 +1,32 @@
 import styled from 'styled-components';
 
-export default function Movie({ movieData, handleDelete }) {
-
-
+export default function Movie({
+  movieData,
+  handleDelete,
+  handleLike,
+  handleDislike,
+  activeBtn,
+}) {
 
   return (
     <MovieStyled>
       <div className="movieCard">
-        <button onClick={() => handleDelete(movieData.id)} className="btnDelete">X</button>
+        <button
+          onClick={() => handleDelete(movieData.id)}
+          className="btnDelete">
+          X
+        </button>
         <h1>{movieData.title}</h1>
         <p className="category">{movieData.category}</p>
         <div className="button">
-          <button>like: {movieData.likes}</button>
-          <button>dislikes: {movieData.dislikes}</button>
+          <button className={`btn ${activeBtn === "like" ? "like-active" : ""}`} onClick={() => handleLike(movieData.id)}>
+            <span className="material-symbols-rounded">thumb_up</span>
+            Like {movieData.likes}
+          </button>
+          <button className={`btn ${activeBtn === "dislike" ? "dislike-active" : ""}`} onClick={() => handleDislike(movieData.id)}>
+            <span className="material-symbols-rounded">thumb_down</span>
+            Dislike {movieData.dislikes}
+          </button>
         </div>
       </div>
     </MovieStyled>
@@ -62,4 +76,37 @@ const MovieStyled = styled.div`
   p {
     color: blue;
   }
+
+  // Button toggle like/dislike
+  
+  .btn-container {
+    display: flex;
+    flex-direction: 'row';
+    align-items: center;
+    background-color: #fff;
+    padding: 30px 50px;
+    border-radius: 12px;
+  }
+ 
+  .btn {
+    border: none;
+    color:#fff;
+    font-size: 0.8rem;
+    display: flex;
+    align-items: center;
+    gap:6px;
+    background-color: #bdc0c5;
+    border-radius: 4px;
+    padding: 6px 15px;
+    margin: 0 10px;
+  }
+  
+  .like-active {
+    background-color: #FF0063;
+  }
+  
+  .dislike-active {
+    background-color: #394350;
+  }
+
 `;
